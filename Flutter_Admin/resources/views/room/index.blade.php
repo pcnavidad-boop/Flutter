e4524cac1af3125df7ddd2c8e8654b04e8ca0b7d
Paul Navidad <161107410+pcsn99@users.noreply.github.com>
2025-11-07 22:16:01 +0800

    test

--- Flutter_Admin/resources/views/room/index.blade.php
+++ Flutter_Admin/resources/views/room/index.blade.php
@@ -7,19 +7,7 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.dataTables.min.css">
 
 <!-- messages -->
-@if (session('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('success') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-@endif
-
-@if (session('error'))
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        {{ session('error') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-@endif
+<x-alert></x-alert>
 
 @if ($errors->any())
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
@@ -31,6 +19,8 @@
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
+
+
 <!-- messages -->
 
 <div class="container mt-5">
